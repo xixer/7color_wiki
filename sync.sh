73f391df9940a7eb88b4364a820e16022c97d0f9
xixer <xixer@126.com>
2012-10-08 17:38:12 +0800

    Automatic commit at 2012/10/08 17:38:12

--- sync.sh
+++ sync.sh
@@ -7,7 +7,7 @@ git pull xixer master
 echo "========================================"
 
 read -p "Pull from server completed, continue to push(y/n)?" push_flag
-if [ "$push_flag" = "y" ]; then
+if [ "$push_flag" != "n" ]; then
     read -p "Enter commit:" commit
     if [ -z $commit ]; then
         mydate=`date '+%x %T'`
@@ -16,7 +16,6 @@ if [ "$push_flag" = "y" ]; then
         commit=$commit
     fi
 
-    #git add -p /data/github.com/wiki/ .
     git add .
     git commit -a -m "$commit"
     git push xixer master
